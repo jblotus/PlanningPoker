2d3007bc0a43a94314b383c90d94ae48051037e5
James Fuller <jblotus@gmail.com>
2014-03-12 04:03:04 +0000

    renamed EstimateRecorder to EstimateTableGateway and implemented some exploratory code

---
+++ src/EstimateTableGateway.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace jblotus\PlanningPoker;
+
+use Aura\Sql\ExtendedPdoInterface;
+
+class EstimateTableGateway
+{
+  private $pdo;
+  
+  public function __construct(ExtendedPdoInterface $pdo)
+  {
+    $this->pdo = $pdo;
+  }
+  
+  public function selectAllFromEstimates()
+  {
+    $statement = "SELECT id, point_value FROM estimates LIMIT 100";
+    $bind = array();
+    return $this->pdo->fetchAll($statement, $bind);
+  }
+  
+  public function insertEstimateWithPoints($pointValue)
+  {
+    $statement = $this->pdo->prepare('INSERT INTO estimates (point_value) VALUES (:point_value)');
+    $statement->bindValue('point_value', $pointValue);
+    $statement->execute();
+    
+    return $this->pdo->lastInsertId();
+  }
+}
--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,18 @@
 <?php
 // autoloader
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
-$estimateRecorder = new jblotus\PlanningPoker\EstimateRecorder();
-echo $estimateRecorder->recordEstimate();
+use Aura\Sql\ExtendedPdo;
+
+$pdo = new ExtendedPdo(
+  'mysql:host=localhost;dbname=pp',
+  'root',
+  ''
+);
+
+$estimateTable = new jblotus\PlanningPoker\EstimateTableGateway($pdo);
+
+$pointValue = mt_rand();
+echo $estimateTable->recordEstimate($pointValue);
+
+echo '<pre>' . print_r($estimateTable->getAllEstimates(), 1) . '</pre>';

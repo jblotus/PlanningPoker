d5abee964347ac27bdce282e57771f55f1ef3142
James Fuller <jblotus@gmail.com>
2014-03-22 03:43:12 +0000

    messing with events

--- views/home.html.php
+++ views/home.html.php
@@ -2,3 +2,11 @@
 <p>
   This is the app.
 </p>
+
+<form role="form" id="pivotal-story-loader">
+  <div class="form-group">
+    <label for="pivotalStoryNumber">Pivotal Tracker Story ID</label>
+    <input type="input" class="form-control" id="pivotalStoryNumber" placeholder="Enter Story #">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
--- webroot/assets/javascript/app.js
+++ webroot/assets/javascript/app.js
@@ -1,1 +1,9 @@
- 
+(function($) {
+  "use strict";
+  $(document).ready(function() {
+    $('#pivotal-story-loader').on('submit', function(e) {
+      e.preventDefault();
+      console.log('enter a id');
+    });
+  });
+}(window.jQuery));

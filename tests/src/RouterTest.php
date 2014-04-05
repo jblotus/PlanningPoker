e2cbedd1c9c79f2568a80f48aee8a492786f4079
James Fuller <jblotus@gmail.com>
2014-04-05 22:34:19 +0000

    added test for router

---
+++ tests/src/RouterTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace jblotus\PlanningPoker;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{ 
+    private $internalRouter;
+    private $request;
+    private $response;
+    private $router;
+    
+    public function setUp()
+    {
+        $this->internalRouter = $this->getMockBuilder('Aura\Router\Router')
+            ->disableOriginalConstructor()
+            ->setMethods(array('add', 'match'))
+            ->getMock();
+
+        $_SERVER['REQUEST_URI'] = 'http://test.com/foo';
+            
+        $webFactory = new \Aura\Web\WebFactory($GLOBALS);
+        
+        $this->request = $webFactory->newRequest();        
+        $this->response = $webFactory->newResponse();
+        
+        $this->router = new Router($this->internalRouter, $this->request, $this->response);
+    }
+    
+    public function testInitializeDoesIt()
+    {        
+        $this->internalRouter->expects($this->at(0))
+            ->method('add')
+            ->with('home', "/");
+        
+        $this->internalRouter->expects($this->at(1))
+            ->method('add')
+            ->with('getPivotalStory', "/get_pivotal_story");
+        
+        $this->internalRouter->expects($this->atLeastOnce())
+            ->method('match')
+            ->with('/foo', $this->request->server->get());
+        
+        $actual = $this->router->initialize();        
+    }
+}

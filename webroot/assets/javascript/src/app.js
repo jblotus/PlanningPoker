7ef19f1b4f9f6e262115416830fa9a4a0e14b475
James Fuller <jblotus@gmail.com>
2014-04-27 19:46:49 +0000

    made js more backboney

--- webroot/assets/javascript/src/app.js
+++ webroot/assets/javascript/src/app.js
@@ -1,16 +1,17 @@
 (function($, _, Backbone, Handlebars) {  
   "use strict";  
   
-  var Story = Backbone.Model.extend({});
+  var Story = Backbone.Model.extend({
+    urlRoot: '/get_pivotal_story'
+  });
   
-  var StoryView = Backbone.View.extend({
+  var CurrentStoryView = Backbone.View.extend({
     el: '#current-story',
     initialize: function() {
       this.listenTo(this.model, 'change', this.render);
     },
     render: function() {      
-      var content = this.template(this.model.toJSON());
-      console.log(content);
+      var content = this.template(this.model.toJSON());      
       this.$el.html(content);
       return this;
     }
@@ -25,38 +26,37 @@
       'submit' : 'submit',
     },
     submit: function(e) {
-      var self = this;
-      console.log(self);
       e.preventDefault();
       var projectId = this.$el.find('input[name=project_id]').val();
-      var storyId = this.$el.find('input[name=story_id]').val();      
+      var storyId = this.$el.find('input[name=story_id]').val();
       
-      var promise = $.ajax({
-        url: '/get_pivotal_story',
-        type: 'get',
+      this.model.fetch({
         data: {
           project_id: projectId,
           story_id: storyId
         }
       });
-      
-      promise.done(function(data) {
-        var story = new Story(data);
-        var storyView = new StoryView({ model: story});          
-        storyView.render();
-      });
     },
     render: function() {
-      console.log('rendering story input view');
       return this;
     }
   });
   
   $(document).ready(function() {
-    var storyViewTemplate = Handlebars.compile($('#story-view-template').html() || '');
-    StoryView.prototype.template = storyViewTemplate;
     
-    var storyInputView = new StoryInputView();    
+    //set up templates
+    var storyViewTemplate = Handlebars.compile($('#story-view-template').html() || '');
+    CurrentStoryView.prototype.template = storyViewTemplate;
+            
+    var currentStoryModel = new Story();
+    
+    var currentStoryView = new CurrentStoryView({
+      model: currentStoryModel
+    });
+    
+    var storyInputView = new StoryInputView({
+      model: currentStoryModel
+    });    
     storyInputView.render();
     
     //temporary autofill

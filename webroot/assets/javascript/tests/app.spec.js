4a018343fa4d8e0a84fba53af97575f265274a07
James Fuller <jblotus@gmail.com>
2014-05-01 04:00:53 +0000

    added test for ajax error handler

--- webroot/assets/javascript/tests/app.spec.js
+++ webroot/assets/javascript/tests/app.spec.js
@@ -1,6 +1,17 @@
 describe('the planning poker app', function() {
   "use strict";
   
+  describe('the ajax error handler', function() {
+    beforeEach(function() {
+      spyOn(window, 'alert');
+    });
+    
+    it('should alert the user with a generic message if the ajax call failed', function() {
+      App.onAjaxError();
+      expect(window.alert).toHaveBeenCalledWith('The was a problem communicating with the server.');
+    });
+  });
+  
   describe('the story model', function() {
     var story;
     beforeEach(function() {

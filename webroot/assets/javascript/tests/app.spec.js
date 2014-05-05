ea401107fb4fb1f46e73176f0958422a52cb61f5
James Fuller <jblotus@gmail.com>
2014-05-05 23:29:28 +0000

    added some tests

--- webroot/assets/javascript/tests/app.spec.js
+++ webroot/assets/javascript/tests/app.spec.js
@@ -11,6 +11,34 @@ describe('the planning poker app', function() {
       expect(window.alert).toHaveBeenCalledWith('The was a problem communicating with the server.');
     });
   });
+    
+  describe('the connected users view', function() {
+    var app,
+        collection;
+    
+    beforeEach(function() {
+      collection = new Backbone.Collection();
+      
+      spyOn(App.ConnectedUsersView.prototype, 'render').andCallThrough();
+      app = new App.ConnectedUsersView({
+        collection: collection
+      });
+    });
+    
+    it('should have #connected-users as default element', function() {
+      expect(App.ConnectedUsersView.prototype.el).toBe('#connected-users');
+    });
+    it('should have a template like', function() {
+      expect(App.ConnectedUsersView.prototype.template({ id: 'foo'})).toBe('<li>foo</li>');
+    });
+    
+    it('should listen to the user collection when something is added or removed', function() {
+      collection.add({ id: 'fooooo' });
+      collection.remove({ id: 'fooooo' });
+      expect(App.ConnectedUsersView.prototype.render).toHaveBeenCalled();
+      expect(App.ConnectedUsersView.prototype.render.callCount).toBe(2);
+    });
+  });
   
   describe('the current vote model', function() {
     var currentVoteModel;

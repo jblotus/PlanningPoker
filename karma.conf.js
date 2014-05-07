5a203b8cbfb966d7a77a2f7e709257e03e41fcf0
James Fuller <jblotus@gmail.com>
2014-05-07 16:01:04 +0000

    removed coveralls from karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'coverage', 'coveralls'],
+    reporters: ['spec', 'coverage'],
     
     coverageReporter: {
       type: 'lcov', // lcov or lcovonly are required for generating lcov.info files

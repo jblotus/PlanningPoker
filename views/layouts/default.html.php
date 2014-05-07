20eedb7877c13afff6a1b230ca3b273e27d6473f
James Fuller <jblotus@gmail.com>
2014-05-07 02:17:37 +0000

    removed extra copy of jquery that was being loaded

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -17,9 +17,7 @@
     <![endif]-->
   </head>
   <body>
-    <?php echo $this->content ?>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <?php echo $this->content ?> 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="/assets/javascript/main.js"></script>
   </body>
